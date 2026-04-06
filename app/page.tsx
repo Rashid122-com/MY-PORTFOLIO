1499e20ceef2f0530d5491332e75b7a1ca0a1b90
Rashid122-com <layrashid123@gmail.com>
2026-04-07 00:33:07 +0500

    fix next app router

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 
 export default function Home() {
+  return <h1>Portfolio is Live 🚀</h1>;
+}
+export default function Home() {
   return (
     <AnimatePresence>
       <motion.main 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // React strict mode for development
+  reactStrictMode: true,
 };
 
 export default nextConfig;
